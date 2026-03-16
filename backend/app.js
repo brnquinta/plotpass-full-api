d6ac67f5bea1d055d67d1ab53d5cdb875b9c55c3
BrunoQ <brn.quinta@gmail.com>
2026-03-16 15:43:05 -0300

    deploy mongodb cloud access

--- backend/app.js
+++ backend/app.js
@@ -4,38 +4,31 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
 const errorMiddleware = require("./middlewares/error");
-
 
 const usersRouter = require("./routes/users");
 const recommendationsRouter = require("./routes/recommendations");
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/plotpassdb")
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use(requestLogger);
 
 app.use("/users", usersRouter);
 app.use("/recommendations", recommendationsRouter);
 
-
 /* Validadores / erros */
 app.use(errorLogger);
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
